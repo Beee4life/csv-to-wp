622301bfdc22f3cef7c876ddf35da75da8eda1fa
Beee <info@berryplasman.com>
2017-09-11 16:02:02 +0200

    Remove stuff so it won't break since there are no actions yet

--- uninstall.php
+++ uninstall.php
@@ -1,22 +1,12 @@
 <?php
-    
+
     // If uninstall.php is not called by WordPress, die
     if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
         die;
     }
-    
-    // Remove Action Logger options
-    $options   = get_option( 'al_available_log_actions' );
-    $options[] = 'available_log_actions';
-    foreach ( $options as $key ) {
-        delete_option( 'al_' . $key );
-    }
-    
-    // If preserve settings is false
-    if ( false == get_option( 'al_preserve_settings' ) ) {
-        /* Delete Action Logger table */
-        global $wpdb;
-        $wpdb->query( "DROP TABLE `" . $wpdb->prefix . "action_logs`" );
-    } else {
-        delete_option( 'al_preserve_settings' );
-    }
+
+    // Remove CSV Importer options
+    // $options   = get_option( 'csvi_' );
+    // foreach ( $options as $key ) {
+    //     delete_option( 'csvi_' . $key );
+    // }
