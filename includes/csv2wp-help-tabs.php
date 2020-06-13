19ad71e3823c944966985786c6ae135278b7209e
Berry Plasman <berry@totaldesign.com>
2020-06-13 15:28:55 +0200

    Fix deprecated filter

--- includes/csv2wp-help-tabs.php
+++ includes/csv2wp-help-tabs.php
@@ -1,22 +1,22 @@
 <?php
-    
+
     /**
      * Add help tabs
      *
-     * @param $old_help  string
-     * @param $screen_id int
-     * @param $screen    object
+     * @param $screen obj
      */
-    function csv2wp_help_tabs( $old_help, $screen_id, $screen ) {
-        
+    function csv2wp_help_tabs( $screen ) {
+
+        // echo '<pre>'; var_dump($screen); echo '</pre>'; exit;
+
         $screens = [
             'toplevel_page_csv2wp-dashboard',
             'admin_page_csv2wp-preview',
             'admin_page_csv2wp-settings',
             'admin_page_csv2wp-support',
         ];
-        
-        if ( 'toplevel_page_csv2wp-dashboard' == $screen_id ) {
+
+        if ( 'toplevel_page_csv2wp-dashboard' == $screen->id ) {
             ob_start();
             ?>
             <h5><?php esc_html_e( 'Upload a CSV file', 'csv2wp' ); ?></h5>
@@ -24,7 +24,7 @@
             <p><?php esc_html_e( 'You can only upload *.csv files.', 'csv2wp' ); ?></p>
             <?php
                 $tab_content = ob_get_clean();
-    
+
                 get_current_screen()->add_help_tab( array(
                     'id'      => 'import-file',
                     'title'   => esc_html__( 'Upload a CSV file', 'csv2wp' ),
@@ -56,7 +56,7 @@
                 'title'   => esc_html__( 'Handle a CSV file', 'csv2wp' ),
                 'content' => $tab_content
             ) );
-    
+
             ob_start();
             ?>
             <h5><?php esc_html_e( 'Import into table', 'csv2wp' ); ?></h5>
@@ -73,12 +73,12 @@
                 'title'   => esc_html__( 'Import table', 'csv2wp' ),
                 'content' => $tab_content
             ) );
-    
+
             ob_start();
             ?>
             <h5><?php esc_html_e( 'Import into meta', 'csv2wp' ); ?></h5>
             <p><?php esc_html_e( 'There are 3 different ways to import post/user meta.', 'csv2wp' ); ?></p>
-            
+
             <p>
                 <b>1. <?php esc_html_e( 'With table headers', 'csv2wp' ); ?></b>
                 <br />
@@ -86,19 +86,19 @@
                 <br />
                 <?php esc_html_e( 'Values must be in the following format: `user id, meta value`.', 'csv2wp' ); ?>
             </p>
-            
+
             <p>
                 <b>2. <?php esc_html_e( 'Without table headers', 'csv2wp' ); ?></b>
                 <br />
                 <?php esc_html_e( 'Must be in the following format: `user id, meta key, meta value`.', 'csv2wp' ); ?>
             </p>
-            
+
             <p>
                 <b>3. <?php esc_html_e( 'Without table headers but with a meta key', 'csv2wp' ); ?></b>
                 <br />
                 <?php esc_html_e( 'Must be in the following format: `user id, meta value`.', 'csv2wp' ); ?>
             </p>
-            
+
             <?php
             $tab_content = ob_get_clean();
 
@@ -107,9 +107,9 @@
                 'title'   => esc_html__( 'Import meta', 'csv2wp' ),
                 'content' => $tab_content
             ) );
-    
-        } elseif ( 'admin_page_csv2wp-preview' == $screen_id ) {
-    
+
+        } elseif ( 'admin_page_csv2wp-preview' == $screen->id ) {
+
             ob_start();
             ?>
             <h5><?php esc_html_e( 'Preview data', 'csv2wp' ); ?></h5>
@@ -117,24 +117,22 @@
             <p><?php esc_html_e( 'Please keep in mind that all csv files are verified before displaying (and therefor can be deleted, when errors are encountered).', 'csv2wp' ); ?></p>
             <p><?php esc_html_e( "If you select 'has header', the first table row will be bolded.", "csv2wp" ); ?></p>
             <p><?php esc_html_e( 'You can limit the amount of lines you want to preview if you have a very large file.', 'csv2wp' ); ?></p>
-    
+
             <?php
             $tab_content = ob_get_clean();
-    
+
             get_current_screen()->add_help_tab( array(
                 'id'      => 'preview-data',
                 'title'   => esc_html__( 'Preview data', 'csv2wp' ),
                 'content' => $tab_content
             ) );
-    
+
         }
-    
-        if ( in_array( $screen_id, $screens ) ) {
-            $sidebar_content = '<p><strong>' . esc_html__( "Author's website", "csv2wp" ) . '</strong></p>';
-            $sidebar_content .= '<p><a href="https://www.berryplasman.com">berryplasman.com</a></p>';
-            get_current_screen()->set_help_sidebar( $sidebar_content );
-        }
-        
-        return $old_help;
+
+        $sidebar_content = '<p><strong>' . esc_html__( "Author's website", "csv2wp" ) . '</strong></p>';
+        $sidebar_content .= '<p><a href="https://berryplasman.com">berryplasman.com</a></p>';
+        get_current_screen()->set_help_sidebar( $sidebar_content );
+
+        // return $old_help;
     }
-    add_filter( 'contextual_help', 'csv2wp_help_tabs', 5, 3 );
+    add_action( 'current_screen', 'csv2wp_help_tabs', 5 );
