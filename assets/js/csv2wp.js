67eeef73958afe66c4a737531873da9a3b6e53b0
Beee <info@berryplasman.com>
2020-12-28 02:48:01 +0100

    Add stylish upload button

--- assets/js/csv2wp.js
+++ assets/js/csv2wp.js
@@ -7,21 +7,31 @@
 
         $('select.csv2wp_import_in').change(function () {
 
-            var changed_value = $(this).val();
+            $changed_value = $(this).val();
 
-            if ( 'table' !== changed_value ){
-                var changed_value = changed_value.substr(4);
+            if ( 'table' !== $changed_value ){
+                $changed_value = $changed_value.substr(4);
                 document.getElementById(header_id).checked = false;
             } else {
                 document.getElementById(header_id).checked = true;
             }
-            console.log(changed_value);
+            var changed_value = $changed_value;
 
             $(table_header).addClass('hidden');
             $(key).addClass('hidden');
             $(table_header + '--' + changed_value).removeClass('hidden');
             $(key + '--' + changed_value).removeClass('hidden');
 
+        });
+
+        $('.upload_button').click(function () {
+            var type = $(this).data('type');
+            // $("#" + type).trigger('click');
+        });
+
+        $("input[type='file']").change(function () {
+            var type = $(this).attr('id');
+            $('.form--' + type + ' .val').text(this.value.replace(/C:\\fakepath\\/i, ''))
         });
     });
 })(jQuery);
