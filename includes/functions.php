a81a19e2c027fedf5ff6b84555d8cf7c9a06fc76
Beee <info@berryplasman.com>
2026-06-08 14:49:07 +0200

    Use wp file delete

--- includes/functions.php
+++ includes/functions.php
@@ -284,7 +284,7 @@
     function csv2wp_delete_file( $file_name, $delete = true ) {
         if ( ! empty( $file_name ) && $delete ) {
             if ( file_exists( csv2wp_get_upload_folder( '/' ) . $file_name ) ) {
-                unlink( csv2wp_get_upload_folder( '/' ) . $file_name );
+                wp_delete_file( csv2wp_get_upload_folder( '/' ) . $file_name );
             }
         }
     }
